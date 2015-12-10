b8bf799fdc47723ee5f6804085229ae168e79d84
Jakub Klapacz <kubaklapacz@gmail.com>
2015-12-10 17:47:35 -0600

    Infrastructure changes

--- hw11/infrastructure.py
+++ hw11/infrastructure.py
@@ -2,12 +2,17 @@ import os
 import numpy as np
 from collections import defaultdict
 
+prefix = "./HMP_Dataset"
+
 def traverse_files():
 	all_arrays = defaultdict(list)
-	for dirname in os.listdir("./HMP_Dataset"):
+	# for dirname in os.listdir("./HMP_Dataset"):
+    for dirname in os.listdir(prefix):
 		# print dirname
 		# continue
-		# if not os.path.isdir(dirname): 
+        actual_name = prefix + "/" + dirname
+        print actual_name
+		# if not os.path.isdir(dirname):
 		# 	print dirname + 'is not a directory'
 		# 	continue
 		for filename in os.listdir(dirname):
@@ -15,7 +20,6 @@ def traverse_files():
 			print "Loading in: ", dirname + os.sep + filename
 			print dirname
 			array = np.loadtxt(dirname + os.sep + filename)
-	 		
-	 		all_arrays[dirname].append(array)
-	 
+            all_arrays[dirname].append(array)
+
 	return all_arrays
